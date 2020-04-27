d3689a12913e68305d100332bd36080a89b8d2aa
esperknight <esperknight@yahoo.com>
2020-04-27 15:12:33 -0400

    Removed unneeded bat files for building graphics.

--- graphics/convert_png2iph.bat
+++
@@ -1,8 +0,0 @@
-for /r %%v in (chip\mod_png\*.png) do tools\PNGtoIPH.exe "%%v"
-move chip\mod_png\*.iph chip\mod_iph
-
-for /r %%v in (etc\mod_png\*.png) do tools\PNGtoIPH.exe "%%v"
-move etc\mod_png\*.iph etc\mod_iph
-
-
-tools\ALDExplorer2
