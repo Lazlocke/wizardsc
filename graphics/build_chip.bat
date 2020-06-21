5ae08d253e6f48c4265373fcd76a6c6f4c704427
esperknight <esperknight@yahoo.com>
2020-06-20 23:57:35 -0400

    Checking in new tool to rebuild VFS files that use the original header to keep the files in order.

--- graphics/build_chip.bat
+++ graphics/build_chip.bat
@@ -2,4 +2,4 @@ del chip.vfs
 del /q chip\mod_vfs
 copy chip\orig_vfs chip\mod_vfs
 tools\image2iph.exe chip\mod_png png chip\mod_vfs
-tools\arc_conv.exe --pack vfs101 chip\mod_vfs chip.vfs
+tools\vfs_build.exe chip\mod_vfs chip.vfs
--- graphics/build_up.bat
+++ graphics/build_up.bat
@@ -2,4 +2,4 @@ del up.vfs
 del /q up\mod_vfs
 copy up\orig_vfs up\mod_vfs
 tools\image2iph.exe up\mod_png png up\mod_vfs
-tools\arc_conv.exe --pack vfs101 up\mod_vfs up.vfs
+tools\vfs_build.exe up\mod_vfs up.vfs
