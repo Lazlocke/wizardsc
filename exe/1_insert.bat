3ef4490f936c46bd97312e48d6badbdfe001d138
esperknight <esperknight@yahoo.com>
2020-07-21 00:14:31 -0400

    Correctly working skills minus job related ones as those are currently breaking the game.

--- exe/1_insert.bat
+++ exe/1_insert.bat
@@ -2,10 +2,12 @@ del error.txt
 del /q ins\*
 copy orig\* ins
 
-echo trans\0005DFBC.txt >> error.txt
-tools\atlas ins\wizard.exe trans\0005DFBC.txt >> error.txt
+
 
 tools\fasm code\wizard-patch.asm wizard.exe
 tools\fasm code\ags-patch.asm Ags.dll
 del /q replacetext.dll
 copy tools\replacetext.dll replacetext.dll
+
+echo trans\0005DFBC.txt >> error.txt
+tools\atlas wizard.exe trans\0005DFBC.txt >> error.txt
